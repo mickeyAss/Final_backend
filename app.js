3ddc3aba7419d4ea9242e9fdce47cd0d373c43e0
micsarawut <65011212206@msu.ac.th>
2025-05-08 01:45:37 +0700

    micup

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ var express = require('express');
 var app = express();
 
 const userRouter =  require('./api/user');
+const categoryRouter =  require('./api/category');
 
 const cors = require('cors');
 var bodyParser = require('body-parser')
@@ -11,5 +12,6 @@ app.use(bodyParser.json());
 app.use(bodyParser.text());
 
 app.use("/user",userRouter);
+app.use("/category",categoryRouter);
 
 module.exports = app;
